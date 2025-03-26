6f09730682f33a2bebbf190b33067fda281d9b14
Junbo Wang <junbowang@email.cugb.edu.cn>
2025-03-26 21:42:54 +0800

    add more questions

--- config/use_study_src.js
+++ config/use_study_src.js
@@ -6,63 +6,64 @@ const Q1_CONFIG = {
     },
     generatedLandscape: {
       A: "static/user_study/Q1/aporee_1472_2019/generated/39400.jpg",
-      B: "static/user_study/Q1/aporee_1472_2019/generated/39405.jpg",
-      C: "static/user_study/Q1/aporee_7104_8811/generated/233005.jpg"
+      B: "static/user_study/Q1/aporee_4062_5420/generated/104139.jpg",
+      C: "static/user_study/Q1/aporee_6420_7965/generated/287782.jpg"
     }
   },
   sub_2: {
     groundTruth: {
-    A: "static/user_study/Q1/aporee_3740_5054/ground_truth/275621.jpg",
-    B: "static/user_study/Q1/aporee_3740_5054/ground_truth/275634.jpg"
+        A: "static/user_study/Q1/aporee_3740_5054/ground_truth/275621.jpg",
+        B: "static/user_study/Q1/aporee_3740_5054/ground_truth/275634.jpg"
     },
     generatedLandscape: {
       A: "static/user_study/Q1/aporee_3740_5054/generated/275621.jpg",
-      B: "static/user_study/Q1/aporee_3740_5054/generated/275634.jpg",
+      B: "static/user_study/Q1/aporee_1472_2019/generated/39405.jpg",
       C: "static/user_study/Q1/aporee_5959_7456/generated/2213.jpg"
     }
   },
   sub_3: {
     groundTruth: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_4091_5449/ground_truth/285917.jpg",
+      B: "static/user_study/Q1/aporee_4091_5449/ground_truth/285926.jpg"
     },
     generatedLandscape: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png",
-      C: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_49356_56256/generated/68447.jpg",
+      B: "static/user_study/Q1/aporee_4091_5449/generated/285926.jpg",
+      C: "static/user_study/Q1/aporee_3740_5054/generated/275634.jpg"
     }
   },
   sub_4: {
     groundTruth: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_6166_7692/ground_truth/138519.jpg",
+      B: "static/user_study/Q1/aporee_6166_7692/ground_truth/138538.jpg"
     },
     generatedLandscape: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png",
-      C: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_6166_7692/generated/138519.jpg",
+      B: "static/user_study/Q1/aporee_7104_8811/generated/233005.jpg",
+      C: "static/user_study/Q1/aporee_49591_56549/generated/55874.jpg"
+
     }
   },
   sub_5: {
     groundTruth: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_49356_56256/ground_truth/68445.jpg",
+      B: "static/user_study/Q1/aporee_49356_56256/ground_truth/68447.jpg",
     },
     generatedLandscape: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png",
-      C: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_49356_56256/generated/68445.jpg",
+      B: "static/user_study/Q1/aporee_4091_5449/generated/285917.jpg",
+      C: "static/user_study/Q1/aporee_6427_7972/generated/33931.jpg"
     }
   },
   sub_6: {
     groundTruth: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_6924_8612/ground_truth/79156.jpg",
+      B: "static/user_study/Q1/aporee_6924_8612/ground_truth/79171.jpg",
     },
     generatedLandscape: {
-      A: "static/images/GISenseLabLogoPNG.png",
-      B: "static/images/GISenseLabLogoPNG.png",
-      C: "static/images/GISenseLabLogoPNG.png"
+      A: "static/user_study/Q1/aporee_49648_56611/generated/133430.jpg",
+      B: "static/user_study/Q1/aporee_6924_8612/generated/79171.jpg",
+      C: "static/user_study/Q1/aporee_49399_56318/generated/149338.jpg"
     }
   },
   sub_7: {
